a1e42867c6f6d0a7e770067c437304e08a4d1441
Chaim Brisk <cbrisk99@gmail.com>
2020-11-05 13:09:58 -0800

    car can turn

---
+++ main.js
@@ -0,0 +1,17 @@
+var myCar = {
+  direction: null
+}
+
+var $car = document.querySelector('img');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === "ArrowDown") {
+    $car.className = 'south';
+  } else if (event.key === "ArrowUp") {
+    $car.className = 'north';
+  } else if (event.key === "ArrowLeft") {
+    $car.className = 'west';
+  } else if (event.key === "ArrowRight") {
+    $car.className = 'east';
+  }
+});
