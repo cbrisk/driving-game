7c515f2d439f1481eaae540583197ed661baa25e
Chaim Brisk <cbrisk99@gmail.com>
2020-11-05 14:29:02 -0800

    minor fix

--- main.js
+++ main.js
@@ -7,15 +7,12 @@ var $car = document.querySelector('img');
 document.addEventListener('keydown', function (event) {
   if (event.key === 'ArrowDown') {
     $car.className = 'south';
-    myCar.direction = 'south';
   } else if (event.key === 'ArrowUp') {
     $car.className = 'north';
-    myCar.direction = 'north';
   } else if (event.key === 'ArrowLeft') {
     $car.className = 'west';
-    myCar.direction = 'west';
   } else if (event.key === 'ArrowRight') {
     $car.className = 'east';
-    myCar.direction = 'east';
   }
+  myCar.direction = $car.className;
 });
