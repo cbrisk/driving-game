1ede14cb51c020595b8f670d72e75d2e91ba5b27
Chaim Brisk <cbrisk99@gmail.com>
2020-11-05 18:03:59 -0800

    extra features

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ var myCar = {
   direction: 'east',
   x: 0,
   y: 0,
-  driving: false
+  driving: false,
+  speed: 0,
+  image: ['images/f1.svg', 'images/fancy.png']
 };
 
 var $car = document.querySelector('img');
+var $background = document.querySelector('div.background');
+var $form = document.querySelector('form');
+var $image = document.querySelector('img');
 var id = null;
 
 document.addEventListener('keydown', function (event) {
@@ -22,16 +27,16 @@ document.addEventListener('keydown', function (event) {
     if (myCar.driving) {
       id = setInterval(function () {
         if (myCar.direction === 'east') {
-          myCar.x += 4;
+          myCar.x += myCar.speed;
           $car.style.left = myCar.x + 'px';
         } else if (myCar.direction === 'west') {
-          myCar.x -= 4;
+          myCar.x -= myCar.speed;
           $car.style.left = myCar.x + 'px';
         } else if (myCar.direction === 'south') {
-          myCar.y += 4;
+          myCar.y += myCar.speed;
           $car.style.top = myCar.y + 'px';
         } else if (myCar.direction === 'north') {
-          myCar.y -= 4;
+          myCar.y -= myCar.speed;
           $car.style.top = myCar.y + 'px';
         }
       }, 16);
@@ -40,4 +45,20 @@ document.addEventListener('keydown', function (event) {
     }
   }
   $car.className = myCar.direction;
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if ($form.elements.car.value === 'luxury') {
+    $image.setAttribute('src', myCar.image[1]);
+  } else {
+    $image.setAttribute('src', myCar.image[0]);
+  }
+  if ($form.elements.speed.value === 'normal') {
+    myCar.speed = 4;
+  } else {
+    myCar.speed = 8;
+  }
+  $form.reset();
+  $background.classList.add('hidden');
 });
